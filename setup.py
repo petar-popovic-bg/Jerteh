d0aedd6db5698ecf21104d50395db01c298d4abe
petar-popovic-bg <petar.popovic.bg@gmail.com>
2021-06-18 15:54:49 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='Jerteh',
     version='0.9',
     packages=['Text', 'Noske', 'Utils', 'Unitex', 'Extraction', 'TreeTagger'],
+    install_requires=[
+        'six==1.16.0',
+        'treetaggerwrapper==2.3'
+    ],
     url='https://github.com/petar-popovic-bg/Jerteh',
     license='GPL',
     author='Petar Popovic',
