1e57353edffe7a55795b6ff3f6560183854ac003
petar-popovic-bg <petar.popovic.bg@gmail.com>
2021-06-18 15:32:28 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Jerteh',
+    version='0.9',
+    packages=['Text', 'Noske', 'Utils', 'Unitex', 'Extraction', 'TreeTagger'],
+    url='https://github.com/petar-popovic-bg/Jerteh',
+    license='GPL',
+    author='Petar Popovic',
+    author_email='petar.popovic.bg@gmail.com',
+    description='Jerteh tools for text processing'
+)
