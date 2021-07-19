c87b38ec72ff5849f4cf58755f0765b734c6b6a0
petar-popovic-bg <petar.popovic.bg@gmail.com>
2021-07-19 01:39:06 -0700

    Added doctrings to Tika

--- Tika/tika.py
+++ Tika/tika.py
@@ -1,8 +1,15 @@
 import tika
 from tika import parser
 
+# Initializes Java Virtual Machine in order to run Tika server.
 tika.initVM()
 
 
 def extract_text(file):
+    """
+    Extract text from a file usinf Apache Tika software which runs on Java. Requires Java runtime to be installed.
+
+    :param file: string (Path to file)
+    :return: string (Extracted text)
+    """
     return parser.from_file(file)['content']
